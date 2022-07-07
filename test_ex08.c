254287429e7ea8126ec97c3c77c07302b7797652
aLeuleu <aurelien.levra@gmail.com>
2022-07-07 18:00:33 +0200

    Add c00'ex08

--- test_ex08.c
+++ test_ex08.c
@@ -2,6 +2,36 @@
 void	ft_print_combn(void);
 int	main(int argc, char **argv)
 {
-	ft_print_combn();
+	ft_print_combn(1);
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(2);
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(3);
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(4);
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(5);
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(6));
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(7);
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(8);
+	write(1,"\n\n\n",3);
+
+	ft_print_combn(9);
+	write(1,"\n\n\n",3);
+	
+	ft_print_combn(9);
+	write(1,"\n\n\n",3);
+
+
 	return (0);
 }

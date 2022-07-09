491e4e693b0fb888bde1c4f619a8e6ddef33407d
aLeuleu <aurelien.levra@gmail.com>
2022-07-09 18:01:16 +0200

    clean

--- test_ex01.c
+++
@@ -1,9 +0,0 @@
-#include<unistd.h>
-void	ft_print_alphabet(void);
-int	main(int argc, char **argv)
-{
-
-	ft_print_alphabet();
-	
-	return (0);
-}
--- test_ex05.c
+++
@@ -1,7 +0,0 @@
-#include<unistd.h>
-void	ft_print_comb(void);
-int	main(int argc, char **argv)
-{
-	ft_print_comb();
-	return (0);
-}

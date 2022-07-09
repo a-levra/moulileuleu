665fe7f732a7e7be046bdd6a2baa26418f28e37c
aLeuleu <aurelien.levra@gmail.com>
2022-07-09 20:53:08 +0200

    fix c01/ex08

--- test_c01/test_ex08.c
+++ test_c01/test_ex08.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 18:18:14 by alevra            #+#    #+#             */
-/*   Updated: 2022/07/07 21:06:32 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/07/09 20:52:46 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,18 @@
 void	ft_putnbr(int nb);
 void	ft_sort_int_tab(int *tab, int size);
 
-int	main(int argc, char **argv)
+int	main()
 {
 	int tab[10] = {1, 1, 0, 4, -5, 6, -7, 8, 999999, 10};
 	int i;
 	char c;
 	i = 0;
 	int res;
-	while(tab[i] != 1)  
+	while(i < 10)  
 		{   
-			c = '0' + tab[i];
-			write(1,&c,1);
+			ft_putnbr(tab[i]);
 			i++; 
 		}
-	ft_putnbr(res);
 	return (0);
 }
 

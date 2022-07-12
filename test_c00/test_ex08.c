d2e93272ae7f057e59ebdd432290c6d74fe07f75
unkn0wn107 <108351913+unkn0wn107@users.noreply.github.com>
2022-07-12 16:40:17 +0200

    Simplifie c00 test_08

--- test_c00/test_ex08.c
+++ test_c00/test_ex08.c
@@ -1,33 +1,15 @@
 #include<unistd.h>
 void	ft_print_combn(int n);
-int	main(int argc, char **argv)
+int	main(void)
 {
-	ft_print_combn(1);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(2);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(3);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(4);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(5);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(6);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(7);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(8);
-	write(1,"\n\n\n",3);
-
-	ft_print_combn(9);
-	write(1,"\n\n\n",3);
+	int	i;
+	
+	i = 0;
+	while(i < 10)
+	{
+		ft_print_combn(i);
+		write(1,"\n\n\n",3);
+	}
 
 	return (0);
 }
